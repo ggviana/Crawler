2ca8dbe5f571bc8802e49ab0637d29d5ebae4f01
Guilherme Guimarães Viana <ggviana@hotmail.com.br>
2013-02-07 11:49:36 -0200

    Revert "09/01"
    This reverts commit bf2461ad68a7193272b232858cb86b6f51b9a5ad.

--- robot.php
+++ robot.php
@@ -13,18 +13,11 @@ class robot{
 	private $userfield;
 	private $passfield;
 	private $cookiefile;
-	private $csv;
 	
 	/**
 		Inicializa um Robo a partir de um arquivo de configuracao
 	*/
 	function __construct($configfile){
-		// Abrindo arquivo CSV
-		$this->csv = fopen("kalunga_".date('Y_m_d-H_i_s').".csv",'a');
-		//$this->csv = fopen("kalunga.csv",'a');
-		$cabecalho = array('cod_produto','Ano','Loja','JAN','FEV','MAR','ABR','MAI','JUN','JUL','AGO','SET','OUT','NOV','DEZ','TOT','EST','MIN','MAX','Sugest','Saldo');
-		fputcsv($this->csv, $cabecalho,";","\"");
-		
 		// Carregar configs
 		$config = parse_ini_file($configfile);
 		
@@ -49,7 +42,6 @@ class robot{
 		Termina o uso do Robo e libera os recursos do sistema
 	*/
 	function terminate($boolean = FALSE){
-		fclose($this->csv);
 		if($boolean === TRUE)
 			clean_session();
 		return curl_close($this->ch);
@@ -77,53 +69,51 @@ class robot{
 	}
 	
 	function post(){
-		$produtos = $this->obter_produtos();
-		foreach($produtos as $produto){
+		/*
+		foreach($this->obter_produtos() as $produto){
 			$produto = $this->separar_produto($produto);
-		
-			//$produto = '212995 Impressora laser color . C330DN# Emb: CX 1 UN CX 1 UN  C_Tabela: 950.211  Ult_Cust: 950.211  UltEnt: 26/12/2012 PVenda: 1349 Condição: 60 DDE                         MD: Sim';
-			$produto = $this->separar_produto($produto);
-			$produto_id = $produto[2];
-			$postfields = array(
-				'Loja' => '----Todas',
-				'CPFJ' => '01619318',
-				'Filial' => '0001',
-				'Feiras' => '',
-				'Produto' => $produto[1],
-				'Linha2' => $produto[3],
-				//'qtdProduto' => '36',
-				/*'arrProd' => 
-					'212995,212996,213009,213028,217708,217816,
-					 218492,218529,220272,220273,220274,220279,
-					 221482,221483,221493,225518,225519,225520,
-					 225521,225526,225530,225536,225537,225538,
-					 225539,225545,225550,225565,225566,225567,
-					 225568,225570,225571,225572,225573,225574'*/
-			);
-			$postfields = http_build_query($postfields);
-			
-			// Configura o cURL para postar
-			set_time_limit(0);
-			curl_setopt ($this->ch, CURLOPT_CONNECTTIMEOUT ,0);
-			curl_setopt ($this->ch, CURLOPT_TIMEOUT, 10000000000);
-			curl_setopt ($this->ch, CURLOPT_POSTFIELDS, $postfields);
-			curl_setopt ($this->ch, CURLOPT_POST, 1);
-			curl_setopt ($this->ch, CURLOPT_URL, 'http://b2b.kalunga.com.br/rvp/GravaSession.asp');
-			$result = $this->limpar(curl_exec($this->ch));
-			$this->analizar($produto_id,$result);
+			//configurar url e curl
 		}
+		*/
+		$produto = '212995 Impressora laser color . C330DN# Emb: CX 1 UN CX 1 UN  C_Tabela: 950.211  Ult_Cust: 950.211  UltEnt: 26/12/2012 PVenda: 1349 Condição: 60 DDE                         MD: Sim';
+		$produto = $this->separar_produto($produto);
+		$postfields = array(
+			'Loja' => '----Todas',
+			'CPFJ' => '01619318',
+			'Filial' => '0001',
+			'Feiras' => '',
+			'Produto' => $produto[1],
+			'Linha2' => $produto[2],
+			//'qtdProduto' => '36',
+			/*'arrProd' => 
+				'212995,212996,213009,213028,217708,217816,
+				 218492,218529,220272,220273,220274,220279,
+				 221482,221483,221493,225518,225519,225520,
+				 225521,225526,225530,225536,225537,225538,
+				 225539,225545,225550,225565,225566,225567,
+				 225568,225570,225571,225572,225573,225574'*/
+		);
+		$postfields = http_build_query($postfields);
+		
+		// Configura o cURL para postar
+		set_time_limit(0);
+		curl_setopt ($this->ch, CURLOPT_CONNECTTIMEOUT ,0);
+		curl_setopt ($this->ch, CURLOPT_TIMEOUT, 10000000000);
+		curl_setopt ($this->ch, CURLOPT_POSTFIELDS, $postfields);
+		curl_setopt ($this->ch, CURLOPT_POST, 1);
+		curl_setopt ($this->ch, CURLOPT_URL, 'http://b2b.kalunga.com.br/rvp/GravaSession.asp');
+		return $this->limpar(curl_exec($this->ch));
 	}
 	
-	function separar_produto($produto){
-		preg_match('/((\d+).*?)\#(.*?$)/', $produto, $casamento);
+	private function separar_produto($produto){
+		preg_match('/(.*?)\#(.*?$)/', $produto, $casamento);
 		return $casamento;
 	}
 	
-	function obter_produtos(){
+	private function obter_produtos(){
 		curl_setopt( $this->ch, CURLOPT_URL, 'http://b2b.kalunga.com.br/rvp/produto.asp' );
 		$result = curl_exec($this->ch);
 		preg_match_all('/<option value=\'(.*?)\'[^>]*>(.*?)<\/option>\r\n/',strip_tags($result, "<option>"),$resultado);
-		file_put_contents('option.html',$resultado[1]);
 		return $resultado[1];
 	}
 	
@@ -144,62 +134,38 @@ class robot{
 		$result = preg_replace('/<!--(.*?)-->/is','',$result);
 		// Reduzindo
 		$result = preg_replace('/ +/is',' ',$result);
-		return $result;
+		return utf8_encode($result);
 	}
 	
-	function analizar($produto_id,$input){
-		// Carregar documento
+	function analizar($input){
 		$doc = new DOMDocument;
 		$doc->loadHTML($input);
 		$xpath = new DOMXPath($doc);
-		$linhas = $xpath->query('//table[2]/tr[position()>4]');
-		$valoresTabela = array();
-		
-		// Leitura do documento
+		$linhas = $xpath->query('//table[2]/tr[position()>3]');
 		foreach($linhas as $linha){
 			if($linha->hasChildNodes()){
-				
 				$filhos = $linha->childNodes;
-				// Capturando o conteudo da linha
-				$valoresLinha = array();
-				for($i=0;$i<($filhos->length/2);$i++){
-					$valorCelula = trim($filhos->item(2*$i)->nodeValue);
-					$valoresLinha[] = $valorCelula;
+				echo "[".($filhos->length)."] ";
+				for($i=0;$i<20;$i++){
+					$val = trim($filhos->item(2*$i)->nodeValue);
+					if($i == 0 AND (int)$val < date('Y'))
+						break 1;
+					echo $val." ";
 				}
-				
-				$valoresLinha = $this->filtrar($valoresLinha);
-				if($valoresLinha !== NULL)
-					array_unshift($valoresLinha,$produto_id);
-
-				$valoresTabela[] = $valoresLinha;
+				echo "\n";
 			}
-		}
-		$this->salvar($valoresTabela);
-	}
-	
-	function filtrar($linha){
-		if(count($linha)<20)return null;
-		//elseif((int)$linha[1] < date('Y'))return null;
-		elseif(preg_match('/(geral|[CD. ]*(barueri|cliente ?ba\w*))/i',$linha[1])) return null;
-		return $linha;
-	}
-
-	function salvar($tabela){
-		foreach($tabela as $linha){
-			fputcsv($this->csv, $linha,";","\"");
+			echo "<br />";
 		}
 	}
-	
 }
-//error_reporting('E_PARSE');
+error_reporting('E_PARSE');
 $oki = new robot('config.ini');
 // Login
 //$result = $oki->login('http://b2b.kalunga.com.br/autentica.asp?act=1000');
 //$oki->clean_session();
-$result = $oki->post();
-//print_r($oki->obter_produtos());
+//$result = $oki->post();
 //file_put_contents('page.html',$result);
 //$oki->analizar($result);
-//$oki->analizar(212995,file_get_contents('page.html'));
+$oki->analizar(file_get_contents('page.html'));
 $oki->terminate();
 ?>
